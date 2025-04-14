9de95561806b60263393bb112b9f121f6cf8213b
LYH513 <peperony367@gmail.com>
2025-04-14 19:52:32 +0900

    Update Entity response & request -> Dto

--- src/main/java/com/example/testcrud/service/DepartmentServiceImpl.java
+++ src/main/java/com/example/testcrud/service/DepartmentServiceImpl.java
@@ -1,9 +1,12 @@
 package com.example.testcrud.service;
 
+import com.example.testcrud.dto.DefaultDto;
+import com.example.testcrud.dto.DepartmentDto;
 import com.example.testcrud.entity.Department;
 import com.example.testcrud.repository.DepartmentRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -17,23 +20,36 @@ public class DepartmentServiceImpl implements DepartmentService {
     }
 
     @Override
-    public Department saveDepartment(Department department) {
-        return departmentRepository.save(department);
+    public DefaultDto.ResDto saveDepartment(DepartmentDto.CreateDepartment department){
+        return departmentRepository.save(department.toEntity()).toCreateResDto();
     }
 
     @Override
-    public List<Department> fetchDepartmentList(){
-        return (List<Department>) departmentRepository.findAll();
+    public List<DepartmentDto.DetailResDto> fetchDepartmentList(){
+        List<Department> list = (List<Department>) departmentRepository.findAll(); // 1. 전체 부서 엔티티 조회
+        List<DepartmentDto.DetailResDto> dtoList = new ArrayList<>();
+        for(Department each : list){
+            dtoList.add(fetchDepartmentById(each.getDepartmentId()));
+        }
+        return dtoList;
     }
 
     @Override
-    public Department fetchDepartmentById(Long departmentId){
-        return departmentRepository.findById(departmentId).orElse(null);
+    public DepartmentDto.DetailResDto fetchDepartmentById(Long departmentId){
+        Department department = departmentRepository.findById(departmentId).orElse(null);
+        DepartmentDto.DetailResDto detailResDto = DepartmentDto.DetailResDto.builder()
+                .departmentId(department.getDepartmentId())
+                .departmentCode(department.getDepartmentCode())
+                .departmentName(department.getDepartmentName())
+                .departmentAddress(department.getDepartmentAddress())
+                .build();
+
+        return detailResDto;
     }
 
     @Override
-    public Department updateDepartment(Department department,
-                                       Long departmentId){
+    public void updateDepartment(DepartmentDto.UpdateReqDto department,
+                                 Long departmentId){
         Department depDB = departmentRepository.findById(departmentId)
                 .orElseThrow(() -> new RuntimeException("Department not found"));
 
@@ -60,7 +76,7 @@ public class DepartmentServiceImpl implements DepartmentService {
 
 
         // save : 엔티티가 존재하면 업데이트, 존재하지 않으면 새로 생성.
-        return departmentRepository.save(depDB);
+        departmentRepository.save(depDB);
     }
 
     @Override
